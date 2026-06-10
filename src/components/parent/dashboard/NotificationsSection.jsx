ff85ca2aed75e998e8fade0eeba632426d1bd5bc
nadaalaa467 <n192004a@gmail.com>
2026-06-10 23:43:18 +0300

    NotificationsSection

--- src/components/parent/dashboard/NotificationsSection.jsx
+++ src/components/parent/dashboard/NotificationsSection.jsx
@@ -1,32 +1,52 @@
-import { Bell } from "lucide-react";
+import bellIcon from "../../../assets/icons/bell-icon.svg"; 
+import bellEmpty from "../../../assets/bell-empty.svg"; 
 
 const NotificationsSection = () => {
   return (
-    // <div className="bg-white border rounded-2xl p-6 h-[260px]">
-    //   <div className="flex items-center gap-2 mb-8">
-    //     <Bell size={18} className="text-blue-700" />
-    //     <h3 className="font-bold text-blue-800">
-    //       الإشعارات
-    //     </h3>
-    //   </div>
+    <div 
+      className="bg-white border border-[#1F293726] rounded-2xl p-8 flex flex-col"
 
-    //   <div className="h-full flex flex-col justify-center items-center">
-    //     <Bell
-    //       size={60}
-    //       className="text-gray-200 mb-4"
-    //     />
+    >
+      <div className="flex items-center justify-start gap-2">
+        <img src={bellIcon} alt="bell" className="w-6 h-6" />
+        <h3 className="font-['IBM_Plex_Sans_Arabic'] font-semibold text-[20px] leading-6 text-[#123C91]">
+          الإشعارات
+        </h3>
+      </div>
 
-    //     <p className="font-semibold">
-    //       لا توجد إشعارات حالياً
-    //     </p>
+      <div className="flex-1 flex flex-col justify-center items-center text-center">
+        <div 
+          className="mb-4 flex items-center justify-center  rounded-full"
+         
+        >
+          <img src={bellEmpty} alt="no-notifications" className="w-20 h-20 opacity-80" />
+        </div>
 
-    //     <p className="text-sm text-gray-500 mt-2">
-    //       ستظهر هنا الإشعارات الخاصة بأبنائك.
-    //     </p>
-    //   </div>
-    // </div>
+        <p 
+          className="font-['IBM_Plex_Sans_Arabic'] text-center mb-2"
+          style={{ 
+            fontWeight: 500, 
+            fontSize: '20px', 
+            lineHeight: '32px', 
+            color: '#1F2937' 
+          }}
+        >
+          لا توجد إشعارات حالياً
+        </p>
 
-    <></>
+        <p 
+          className="font-['IBM_Plex_Sans_Arabic'] text-center "
+          style={{ 
+            fontWeight: 400, 
+            fontSize: '16px', 
+            lineHeight: '24px', 
+            color: '#1F2937BF' 
+          }}
+        >
+          ستظهر هنا الإشعارات الخاصة بأبنائك.
+        </p>
+      </div>
+    </div>
   );
 };
 
--- src/pages/parent/Home.jsx
+++ src/pages/parent/Home.jsx
@@ -12,8 +12,9 @@ const Home = () => {
         <StatsCards />
 
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+           <UpcomingLessonsSection />
           <NotificationsSection />
-          <UpcomingLessonsSection />
+         
         </div>
       </div>
     </ParentLayout>
