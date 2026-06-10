5b2eba0df390e632c72e54734c67ab1339d05070
nadaalaa467 <n192004a@gmail.com>
2026-06-10 23:34:24 +0300

    UpcomingLessonsSection

--- src/components/parent/dashboard/UpcomingLessonsSection.jsx
+++ src/components/parent/dashboard/UpcomingLessonsSection.jsx
@@ -1,32 +1,56 @@
-import { Calendar } from "lucide-react";
+import calendarIcon from "../../../assets/icons/calendar-icon.svg";
+import calendarEmpty from "../../../assets/calendar-empty.svg";
 
 const UpcomingLessonsSection = () => {
   return (
-    // <div className="bg-white border rounded-2xl p-6 h-[260px]">
-    //   <div className="flex items-center gap-2 mb-8">
-    //     <Calendar size={18} className="text-blue-700" />
-    //     <h3 className="font-bold text-blue-800">
-    //       الدروس القادمة
-    //     </h3>
-    //   </div>
+    <div
+      className="bg-white border border-[#1F293726] rounded-2xl p-8 flex flex-col"
 
-    //   <div className="h-full flex flex-col justify-center items-center">
-    //     <Calendar
-    //       size={60}
-    //       className="text-gray-200 mb-4"
-    //     />
+    >
+      <div className="flex items-center justify-start gap-2 mb-6 w-full">
+        <img src={calendarIcon} alt="calendar" className="w-6 h-6" />
 
-    //     <p className="font-semibold">
-    //       لا توجد دروس حالياً
-    //     </p>
+        <h3
+          className="font-['IBM_Plex_Sans_Arabic'] font-semibold text-[20px] leading-6 text-[#123C91]"
+          style={{ letterSpacing: '0px' }}
+        >
+          الدروس القادمة
+        </h3>
+      </div>
 
-    //     <p className="text-sm text-gray-500 mt-2">
-    //       أضف أبنائك لعرض الجدول الدراسي.
-    //     </p>
-    //   </div>
-    // </div>
+      <div className="flex-1 flex flex-col justify-center items-center text-center">
+        <div
+          className="mb-4 flex items-center justify-center  rounded-full"
 
-    <></>
+        >
+          <img src={calendarEmpty} alt="no-lessons" className="w-20 h-20 opacity-80" />
+        </div>
+
+        <p
+          className="font-['IBM_Plex_Sans_Arabic'] text-center mb-2"
+          style={{
+            fontWeight: 500,
+            fontSize: '20px',
+            lineHeight: '32px',
+            color: '#1F2937'
+          }}
+        >
+          لا توجد دروس حالياً
+        </p>
+
+        <p
+          className="font-['IBM_Plex_Sans_Arabic'] text-center "
+          style={{
+            fontWeight: 400,
+            fontSize: '16px',
+            lineHeight: '24px',
+            color: '#1F2937BF'
+          }}
+        >
+          أضف أبنائك أولاً لعرض الجدول الدراسي و الدروس القادمة.
+        </p>
+      </div>
+    </div>
   );
 };
 
