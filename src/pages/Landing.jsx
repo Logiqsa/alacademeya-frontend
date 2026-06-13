af3d5831b36779433ad74a5bcbc85440b0b98fa4
nadaalaa467 <n192004a@gmail.com>
2026-06-13 22:12:35 +0300

    update

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,9 @@
 import Hero from "../components/landing/Hero";
 import Features from "../components/landing/Features";
 import Services from "../components/landing/Services";
-// import Stats from "../components/landing/Stats";
+import Stats from "../components/landing/Stats";
 import Marquee from "../components/landing/Marquee";
-// import FAQ from "../components/landing/FAQ";
+import FAQ from "../components/landing/FAQ";
 import Pricing from "../components/landing/Pricing";
 
 export default function Landing() {
@@ -14,8 +14,8 @@ export default function Landing() {
       <Features />
       <Services />
       <Pricing />
-      {/* <Stats /> */}
-      {/* <FAQ /> */}
+      <Stats />
+      <FAQ />
     </>
   );
 }
