238275df3e2653d70c932b31b34e9adb13c4542c
nadaalaa467 <n192004a@gmail.com>
2026-07-18 01:13:44 +0300

    SchedulePage

--- src/pages/admin/SchedulePage.jsx
+++ src/pages/admin/SchedulePage.jsx
@@ -1,8 +1,10 @@
 import AdminLayout from "../../components/admin/layout/AdminLayout";
 import MonthlySchedule from "../../components/schedule/MonthlySchedule";
+import Breadcrumbs from "../shared/Breadcrumbs";
 
 const SchedulePage = () => (
   <AdminLayout>
+    <Breadcrumbs homeTo="/admin-dashboard" />
     <MonthlySchedule role="admin" title="جدول الحصص" subtitle="متابعة جميع حصص الفصول النشطة خلال الشهر." />
   </AdminLayout>
 );
